292c49f08b0cb9f481f0741a8e52288ac5e8142b
wyattscarpenter <wyattscarpenter@gmail.com>
2025-06-29 11:22:25 -0700

    literally copy over https://github.com/wyattscarpenter/streamlit-profiler/blob/42d49e9068278ff43aa51100bc982f669926cf1e/typecheck.py

--- src/typegun/__init__.py
+++ src/typegun/__init__.py
@@ -1,2 +1,48 @@
+#!/usr/bin/env python3
+
+"""This is a fairly generic script to run typecheckers for you automatically.
+It was initially made by Wyatt S Carpenter and carted around between projects.
+The license of this file is public domain, in addition to any other licenses stuck on it."""
+
+from os import system as s, name as os_name
+from sys import argv
+
+def pause() -> None:
+    input("Press enter to exit...")
+
+def h(string: str) -> None:
+    """Print a header for a typechecker section"""
+    print(f"---- {string.upper()}:")
+
+def hs(header: str, system_command: str) -> int:
+    """Print a header, then run a system command (returning its return code)."""
+    h(header)
+    return s(system_command)
+
 def main() -> None:
-    print("Hello from typegun!")
+    if "-h" in argv or "--help" in argv:
+        print("Just run this script to check the types.")
+        return 
+
+    hs("uv", "uv --version --verbose || pip install uv --disable-pip-version-check --break-system-packages")
+    print("Trying to install requirements.txt as a preemptive fallback in case there is no pyproject.toml...") 
+    s("uv pip install -r requirements.txt")
+    s("uv pip install ruff pyright mypy pytype pip")
+    # One of these next two lines will work, depending on your operating system...
+    if os_name == 'nt':
+        s("set PYRIGHT_PYTHON_FORCE_VERSION=latest")
+    else:
+        s("export PYRIGHT_PYTHON_FORCE_VERSION=latest")
+
+    hs("ruff", "uv run ruff check --no-cache")
+    hs("pyright", "uv run pyright")
+    h("mypy")
+    #s("uv run mypy --install-types --non-interactive")
+    s("uv run mypy . --strict --pretty --warn-unreachable --strict-bytes --enable-error-code explicit-override")
+    h("pytype")
+    s("uv run pytype .")
+    print("---- fin")
+    pause()
+
+if __name__ == "__main__":
+    main()
